5d905291ee81f897303d146bfb0513adc8549503
nibudd <nibudd@gmail.com>
2022-05-07 00:47:50 -0400

    started using template literals and added coordinates to move buttons

--- src/historical-moves.js
+++ src/historical-moves.js
@@ -5,11 +5,16 @@ export class HistoricalMoves extends React.Component {
   render() {
     const history = this.props.history;
     const moves = history.map(
-      (board, move) => {
-        const description = move ? "Go to move #" + move : "Go to game start";
+      ({squarePlayed, player}, move_number) => {
+        const row = Math.floor(squarePlayed / 3);
+        const col = squarePlayed % 3;
+        const description = move_number ? 
+          `Go to move # ${move_number}: ${player} @ (${row}, ${col})`: 
+          "Go to game start";
+
         return (
-          <li key={move}>
-            <button onClick={() => this.props.onClick(move)}>{description}</button>
+          <li key={move_number}>
+            <button onClick={() => this.props.onClick(move_number)}>{description}</button>
           </li>
         );
       }
