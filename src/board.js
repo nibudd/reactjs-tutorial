6c31d78cf5b4941b175089c39455e887b7231d76
nibudd <nibudd@gmail.com>
2022-06-15 20:16:01 -0400

    create board with loops

--- src/board.js
+++ src/board.js
@@ -13,24 +13,18 @@ export class Board extends React.Component {
   }
   
   render() {
+    const boardRows = [];
+    for (let i = 0; i < 3; i++) {
+      const boardRow = [];
+      for (let j = 0; j < 3; j++) {
+        const squareNumber = i * 3 + j;
+        boardRow.push(this.renderSquare(squareNumber));
+      }
+      boardRows.push(<div className="board-row">{boardRow}</div>)
+    }
+    
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div>{boardRows}</div>
     );
   }
 }
